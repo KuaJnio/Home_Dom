203f4a085558313bed0cee6d8d051ab5502595fa
Romain DUPONT <3.romain.dupont@gmail.com>
2017-11-15 20:32:52 +0100

    mqtt over websocket

--- mqtt/build.sh
+++ mqtt/build.sh
@@ -6,7 +6,7 @@ TARGET="homedom-${ARCH}-touch"
 REGISTRY="registry:5000"
 IMAGE="${REGISTRY}/${ARCH}-${NAME}"
 REMOTE="docker -H ${TARGET}:2375"
-RUN="run -d --restart always --name ${NAME} -p 1883:1883 ${IMAGE}"
+RUN="run -d --restart always --name ${NAME} -p 1883:1883 -p 1884:1884 ${IMAGE}"
 
 docker build --no-cache --pull -t ${IMAGE} .
 docker push ${IMAGE}
