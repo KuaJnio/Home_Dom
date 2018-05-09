0ab623079609b235f7b81529a93944bda5d73681
Romain DUPONT <3.romain.dupont@gmail.com>
2018-05-09 14:52:22 +0200

    removed local js and import from cdns

--- webapp/source/static/mqtt.js
+++ webapp/source/static/mqtt.js
@@ -11,31 +11,31 @@ client.connect({onSuccess:onConnect});
 
 // called when the client connects
 function onConnect() {
-  // Once a connection has been made, make a subscription and send a message.
-  console.log("onConnect");
-  client.subscribe("#");
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("onConnect");
+    client.subscribe("#");
 }
 
 // called when the client loses its connection
 function onConnectionLost(responseObject) {
-  if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
-  }
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:"+responseObject.errorMessage);
+    }
 }
 
 // called when a message arrives
 function onMessageArrived(message) {
-  console.log("onMessageArrived:"+message.payloadString);
+    console.log("onMessageArrived:"+message.payloadString);
 }
 
 $( ".on" ).click(function() {
-  message = new Paho.MQTT.Message(JSON.stringify({"HD_FEATURE": "HD_BUTTON", "HD_IDENTIFIER": this.id, "HD_VALUE": 1}));
-  message.destinationName = "inputs";
-  client.send(message);
+    message = new Paho.MQTT.Message(JSON.stringify({"HD_FEATURE": "HD_BUTTON", "HD_IDENTIFIER": this.id, "HD_VALUE": 1}));
+    message.destinationName = "inputs";
+    client.send(message);
 });
 
 $( ".off" ).click(function() {
-  message = new Paho.MQTT.Message(JSON.stringify({"HD_FEATURE": "HD_BUTTON", "HD_IDENTIFIER": this.id, "HD_VALUE": 0}));
-  message.destinationName = "inputs";
-  client.send(message);
+    message = new Paho.MQTT.Message(JSON.stringify({"HD_FEATURE": "HD_BUTTON", "HD_IDENTIFIER": this.id, "HD_VALUE": 0}));
+    message.destinationName = "inputs";
+    client.send(message);
 });
