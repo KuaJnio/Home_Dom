744d218a291307fe07b1a84c746c786a64910dcb
Romain DUPONT <romain.dupont@caramail.fr>
2019-01-19 00:03:37 +0100

    modif get_config

--- enocean/source/get_config.py
+++ enocean/source/get_config.py
@@ -5,7 +5,7 @@ import logging
 def get_parameter(key):
     try:
         logging.info("Getting parameter {}...".format(key))
-        r = requests.get("http://192.168.1.16:8090/", timeout=1)
+        r = requests.get("http://192.168.1.16:8090/", timeout=30)
         return r.json().get(key, None)
     except Exception as e:
         logging.error("{}".format(e))
